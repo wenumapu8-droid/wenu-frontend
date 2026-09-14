80d7f1329609a9e45d9c5f3c7959deee0a57cda9
wenumapu8-droid <wenu.mapu8@gmail.com>
2026-09-13 17:33:36 -0700

    feat(kdx-engine): export device, state and memory adapters

--- src/kodex/engine/index.js
+++ src/kodex/engine/index.js
@@ -3,3 +3,16 @@ export { KDX_RENDERER_CONTRACT_VERSION, assertKdxRenderer, describeKdxRenderer }
 export { KDX_EXPERIENCE_PRESETS, getKdxExperiencePreset, validateKdxExperiencePreset } from './kdxExperiencePresets.js';
 export { KDX_ENGINE_PROFILE, KdxEngineCore } from './kdxEngineCore.js';
 export { KodexWorldRendererAdapter } from './kodexWorldAdapter.js';
+export {
+  normalizePointerCoordinates,
+  mountPointerSignalAdapter,
+  mountTouchSignalAdapter,
+  mountKeyboardSignalAdapter,
+  createAudioSignalAdapter,
+} from './kdxDeviceAdapters.js';
+export { KDX_SCENE_STATE_TO_WORLD_PHASE, sceneStateToWorldPhase, createKdxSceneStateBridge } from './kdxStateBridge.js';
+export {
+  KDX_ORGANISM_ACTION_EVENT,
+  createKdxJourneyCommitAdapter,
+  createBrowserKdxJourneyCommitAdapter,
+} from './kdxJourneyCommitAdapter.js';
